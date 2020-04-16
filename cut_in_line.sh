b45b1ed12f54ae10ad23d94ca074df62096aceb3
Bobholamovic <bob1998425@hotmail.com>
2020-04-16 21:43:32 +0800

    Change order of visible devices

--- cut_in_line.sh
+++ cut_in_line.sh
@@ -2,7 +2,7 @@
 
 THRESH=5000 
 
-CUDA_VISIBLE_DEVICES=$(nvidia-smi | sed -n -E 's/.* ([0-9]+)MiB \/ ([0-9]+)MiB .*/\1 \2/p' | awk '$2-$1>'"$THRESH"'{printf "%d,",NR-1}')
+CUDA_VISIBLE_DEVICES=$(nvidia-smi | sed -n -E 's/.* ([0-9]+)MiB \/ ([0-9]+)MiB .*/\1 \2/p' | awk '$2-$1>'"$THRESH"'{printf "%d,%d\n",NR-1,$2-$1}' | sort -rk2 | awk '{printf "%d,",$1}')
 export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES%,}
 
 python -c "import os; print(os.environ['CUDA_VISIBLE_DEVICES'])"
